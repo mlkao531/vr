380a6a21f460005573801fa77b7d696ae35a2d30
mlkao531 <michaelkao0531@gmail.com>
2024-12-12 11:19:11 +0800

    0000

---
+++ 外面/app-files/data.js
@@ -0,0 +1,286 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-7-11",
+      "name": "7-11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.6653151378809055,
+        "pitch": 0.15154367762137433,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.27072110719827,
+          "pitch": 0.5832101546782376,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.620342169508194,
+          "pitch": 0.380491066391512,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6736027180424875,
+          "pitch": -0.3098615864934615,
+          "title": "7-11",
+          "text": "人超多"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "大潤發",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.024561064073937544,
+          "pitch": 0.49356527671423933,
+          "rotation": 3.141592653589793,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.05884529979952191,
+          "pitch": 0.08215054055430215,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1550998698444523,
+          "pitch": -0.5015413243595646,
+          "title": "大潤發",
+          "text": "上學必經之路<div>但很少學生消費</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "公車站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.0000011929000010013624,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4076662311865284,
+          "pitch": 0.461761366950423,
+          "rotation": 0.7853981633974483,
+          "target": "0-7-11"
+        },
+        {
+          "yaw": -0.0012398336016268274,
+          "pitch": 0.1518073151618875,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1861838413811903,
+          "pitch": 0.12073565105949058,
+          "title": "公車站",
+          "text": "最常使用的是706，707，705三路公車"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "幼兒園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.093867251505115,
+          "pitch": 0.20120662073843576,
+          "rotation": 4.71238898038469,
+          "target": "0-7-11"
+        },
+        {
+          "yaw": -2.436365718080854,
+          "pitch": 0.2076766416643494,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9423581045845122,
+          "pitch": -0.04859277432174025,
+          "title": "幼兒園",
+          "text": "今年才蓋完<div>很新</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "捷運站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.961591018948493,
+          "pitch": 0.11250426491601928,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.9216018896534344,
+          "pitch": 0.04588391560765359,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6325758165024338,
+          "pitch": -0.17998545955738798,
+          "title": "捷運站",
+          "text": "BL02永寧<div>只能搭往頂埔的車</div>"
+        }
+      ]
+    }
+  ],
+  "name": "外面",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
