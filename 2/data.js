9cb3553c61644be9f45f75e9d9dd7651fa077448
mlkao531 <michaelkao0531@gmail.com>
2024-12-19 11:27:25 +0800

    Update data.js

--- 2/data.js
+++ 2/data.js
@@ -290,7 +290,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
