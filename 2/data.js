67066a012c647610340272d8925dd07fb3ef4917
mlkao531 <michaelkao0531@gmail.com>
2024-12-19 10:48:42 +0800

    werk;ljko;ewfklj;;lkmrgm;lkrg

---
+++ 2/data.js
@@ -0,0 +1,296 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "保安宮",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2267041884456482,
+          "pitch": 0.11811185213808528,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.08208406772172,
+          "pitch": -0.3602414884572127,
+          "title": "保安宮",
+          "text": "數理資優班及科學社的傳統會在會考前帶來參拜<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "活動中心",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07920354611277247,
+          "pitch": 0.24930463197798858,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.6154517760330407,
+          "pitch": 0.3829547700137894,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2446929581605168,
+          "pitch": -0.25883393506290986,
+          "title": "活動中心",
+          "text": "童軍家政課會來這裡搭帳蓬<div>占地大但是使用率不高</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "教室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0141713305240696,
+          "pitch": -0.07784140431928677,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.9898616323826026,
+          "pitch": -0.03358610510301219,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.025084810604937502,
+          "pitch": -0.0700896932338253,
+          "title": "教室",
+          "text": "現已改成電子白板<div>後面走廊為了安全(同時保證學生不會跑掉)裝設鐵捲門</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "腳踏車",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1044138274702275,
+          "pitch": 0.14804461658745538,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.9793366686469405,
+          "pitch": 0.397311888386346,
+          "rotation": 11.780972450961727,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.2976653907206828,
+          "pitch": 0.4256456568761422,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9827779874205937,
+          "pitch": 0.0839084656365987,
+          "title": "腳踏車站",
+          "text": "目前維修中"
+        },
+        {
+          "yaw": -0.9060387024963177,
+          "pitch": -0.24108968288111043,
+          "title": "後面的山",
+          "text": "有桐花步道"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "操場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5425579402793703,
+          "pitch": 0.23152706531663725,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.2884003128458126,
+          "pitch": 0.11885537910675481,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.2789216910327639,
+          "pitch": -0.041554253854220846,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7121882853081978,
+          "pitch": 0.07504578237091408,
+          "title": "操場",
+          "text": "現在有四個半場改成光電球場"
+        },
+        {
+          "yaw": 0.023391449298893363,
+          "pitch": -0.3125854091917901,
+          "title": "綜合大樓",
+          "text": "大型活動所在地"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
